f68c0d2444ea70216e6ba7b1d651fc308e6a26fb
robf-telicent <rob.fisher@telicent.io>
2026-06-26 16:28:23 +0100

    fix: initdb now assigns schema ownership (#626)

--- charts/demo-prereqs/charts/postgres/scripts/initdb.sh
+++ charts/demo-prereqs/charts/postgres/scripts/initdb.sh
@@ -15,3 +15,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER ${AUTH_DB_USER} WITH PASSWORD '${AUTH_DB_PASSWORD}';
     GRANT ALL PRIVILEGES ON DATABASE auth TO ${AUTH_DB_USER};
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "user_preferences" <<-EOSQL
+    GRANT ALL ON SCHEMA public TO ${USER_PREFS_DB_USER};
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "auth" <<-EOSQL
+    GRANT ALL ON SCHEMA public TO ${AUTH_DB_USER};
+EOSQL
